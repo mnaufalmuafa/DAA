5ddbea222fd8afe27d975169e33223fb781c1821
mnaufalmuafa <naufalmuafa77@gmail.com>
2020-04-22 08:13:08 +0700

    Menyelesaikan encoding

--- huffman.py
+++ huffman.py
@@ -1,5 +1,6 @@
 DEBUG = True
-string = "aabbccddeeeeffffffgggggggg"
+string = "acffecdefeffdea"
+string = input("Teks : ")
 
 class NodeTree(object):
     def __init__(self, left=None, right=None):
@@ -27,7 +28,7 @@ def huffmanCodeTree(node, left=True, binString=""):
     return d
 
 freq = {}
-for c in string:
+for c in string: #Mencari frekuensi dari setiap karakter
     if c in freq:
         freq[c] += 1
     else:
@@ -39,19 +40,44 @@ freq = sorted(freq.items(), key=lambda x: x[1], reverse=True)
 
 nodes = freq
 
-#Membaat Tree
+#Membuat Tree
 while len(nodes) > 1:
     key1, c1 = nodes[-1]
     key2, c2 = nodes[-2]
     nodes = nodes[:-2]
     node = NodeTree(key1, key2)
     nodes.append((node, c1 + c2))
-    # Mengurutkan kembali list
     nodes = sorted(nodes, key=lambda x: x[1], reverse=True)
 
 huffmanCode = huffmanCodeTree(nodes[0][0])
 
+tabel = []
+print("")
+print("Teks : ", string)
+print()
 print (" Char | Freq  | Huffman code ")
 print ("-----------------------------")
 for char, frequency in freq:
-    print( " %-4r | %5d | %12s" % (char, frequency, huffmanCode[char]))
+    tabel.append((char,frequency,huffmanCode[char]))
+
+arrChar = list(string)
+i = 0
+while i < len(tabel) : #Menampilkan frekuensi dan kode Huffman
+    char, frequency, kode = tabel[i]
+    print(" %-4r | %5d | %12s" % (char, frequency, huffmanCode[char]))
+    i = i + 1
+
+kodeHuffman = ""
+i = 0
+while i < len(arrChar) : #Membuat hasil Encoding untuk string input
+    j = 0
+    while j < len(tabel) :
+        char = tabel[j][0]
+        if char == arrChar[i] :
+            break
+        j = j + 1
+    kodeHuffman = kodeHuffman + tabel[j][2]
+    i = i + 1
+
+print()
+print("Seteleh Encoding : ", kodeHuffman)
